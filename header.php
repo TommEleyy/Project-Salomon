5d40b9c682591f382bff84a4b64f6539bf9c974b
TommEleyy <thomas.l.salomon@gmail.com>
2018-04-25 16:05:55 +0200

    start-of-the-project

---
+++ header.php
@@ -0,0 +1,49 @@
+<?php
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?php echo (isset($title))?$title:"Salomon"; ?></title>
+	<meta name="description" content="">
+
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+	<div class="logo">
+		<center>
+			<a href="index.php"><img src="images/_T_logo.svg.png" width="170px" alt="Logo TommEleyy" class="zoom"></a>
+		</center>
+	</div>
+	<nav class="navbar navbar-expand-lg navbar-light bg-nav">
+
+	  <div class="collapse navbar-collapse" id="navbarNav">
+	  	<div class="menu">
+		    <ul class="navbar-nav">
+		      <li class="nav-item active">
+		        <a class="nav-link zoom" href="index.php"><b>ACCUEIL</b></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link zoom" href="diplomes.php"><b>Diplômes</b></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link zoom" href="scolarite.php"><b>Scolarité</b></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link zoom" href="projets_missions.php"><b>Projets & Missions réalisées</b></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link zoom" href="contact.php"><b>Contact</b></a>
+		      </li>
+		    </ul>
+		  </div>
+	  </div>
+
+	</nav>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+	require_once "header.php";
+?>
+
+
+<div class="container-fluid-intro">
+	<div class="intro">
+	<center>
+	<p class="text-intro">Bienvenu </p>
+	</center>
+	</div>
+</div>
+
+
+
+
+
+
+<?php
+	include "footer.php";
+?>
